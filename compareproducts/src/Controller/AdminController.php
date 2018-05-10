9299134e4124ed0d11223fc7e72e04e7326be044
Wouter Wijsman <wwijsman@live.nl>
2018-05-10 21:13:29 +0200

    You can now add and remove stores

--- compareproducts/src/Controller/AdminController.php
+++ compareproducts/src/Controller/AdminController.php
@@ -9,20 +9,58 @@
 namespace App\Controller;
 
 
+use App\Entity\Store;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
+
     /**
-     * @Route("/admin/store")
+     * @param EntityManagerInterface $entityManager
+     * @param Request $request
+     * @return Response
+     * @Route("/admin/store/remove")
      */
-    public function addStores() : Response
+    public function removeStore(EntityManagerInterface $entityManager, Request $request) : Response
     {
-        return $this->render('admin/store.html.twig', array());
+
+        $toDelete = $request->request->get("remove", "");
+        $store = $entityManager->getRepository(Store::class)->findOneBy(array('id' => $toDelete));
+        $entityManager->remove($store);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("storeAdmin");
     }
 
+    /**
+     * @Route("/admin/store", name="storeAdmin")
+     */
+    public function overviewStores(EntityManagerInterface $entityManager, Request $request) : Response
+    {
+        $newName = $request->request->get("name", "");
+        $newUrl = $request->request->get("url", "");
+        $toDelete = $request->request->get("remove", "");
+
+        if(!empty($newUrl) && !empty($newUrl)){
+            $store = new Store();
+            $store->setName($newName);
+            $store->setUrl($newUrl);
+            $entityManager->persist($store);
+            $entityManager->flush();
+        } elseif(!empty($toDelete)) {
+            $store = $entityManager->getRepository(Store::class)->findOneBy(array('id' => $toDelete));
+            $entityManager->remove($store);
+        }
+
+        $stores = $entityManager->getRepository(Store::class)->findAll();
+        return $this->render('admin/store.html.twig', array(
+            'stores' => $stores,
+        ));
+    }
 
     /**
      * @Route("/admin")
